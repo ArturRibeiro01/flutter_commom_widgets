8ff4d64a810644c0520706f73f8fac9e6b914b7b
Artur Ribeiro <profissional.arturrc@gmail.com>
2023-12-10 23:34:59 -0300

    feat: row

--- lib/my_home_page.dart
+++ lib/my_home_page.dart
@@ -16,38 +16,60 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 131),
-            child: Container(
-              height: 150,
-              width: 150,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Colors.amber,
-                  image: const DecorationImage(
-                      image: NetworkImage(
-                          'https://avatars.githubusercontent.com/u/59667445?v=4'),
-                      fit: BoxFit.cover),
-                  border: Border.all(color: Colors.brown, width: 2)),
-              child: const Center(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Text(
-                    'Artur Ribeiro',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.brown,
-                        fontWeight: FontWeight.bold),
-                  ),
-                ),
-              ),
-            ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.blueGrey,
+            width: 100,
+            height: 580,
           ),
-        ),
+          Container(
+            color: Colors.cyanAccent,
+            width: 100,
+            height: 480,
+          ),
+          Container(
+            color: Colors.redAccent,
+            width: 100,
+            height: 480,
+          ),
+        ],
       ),
+
+      // body: Center(
+      //   child: Align(
+      //     alignment: Alignment.topCenter,
+      //     child: Padding(
+      //       padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 131),
+      //       child: Container(
+      //         height: 150,
+      //         width: 150,
+      //         decoration: BoxDecoration(
+      //             shape: BoxShape.circle,
+      //             color: Colors.amber,
+      //             image: const DecorationImage(
+      //                 image: NetworkImage(
+      //                     'https://avatars.githubusercontent.com/u/59667445?v=4'),
+      //                 fit: BoxFit.cover),
+      //             border: Border.all(color: Colors.brown, width: 2)),
+      //         child: const Center(
+      //           child: Align(
+      //             alignment: Alignment.bottomCenter,
+      //             child: Text(
+      //               'Artur Ribeiro',
+      //               style: TextStyle(
+      //                   fontSize: 18,
+      //                   color: Colors.brown,
+      //                   fontWeight: FontWeight.bold),
+      //             ),
+      //           ),
+      //         ),
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
